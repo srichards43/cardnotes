f35399de1939b4323839360f7fc5de127910780f
Scott Richards <sr845@kent.ac.uk>
2026-08-16 21:19:40 +0100

    UI improvements and added renaming and deletion of collections

--- src/components/CollectionCard.tsx
+++ src/components/CollectionCard.tsx
@@ -1,16 +1,83 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import './CollectionCard.css'
 
 type CollectionCardData = {
     id: string
     title: string
-    noteCount: number
+    menuOpen: boolean
+    onMenuToggle: () => void
+    onDelete: (id: string) => void
+    onRename: (id: string, newTitle: string) => void
 }
 
-function CollectionCard({ id, title, noteCount }: CollectionCardData) {
+function CollectionCard({ id, title, menuOpen, onMenuToggle, onDelete, onRename }: CollectionCardData) {
+    const [editing, setEditing] = useState(false)
+
+    function saveTitle(newTitle: string) {
+        newTitle = newTitle.trim()
+
+        if (newTitle !== '') {
+            onRename(id, newTitle)
+        }
+
+        setEditing(false)
+    }
+    
     return (
         <Link to={`/collection/${id}`} className="collection-card">
-            <h2>{title}</h2>
-            <p>{noteCount} notes</p>
+            <div className="vlg">
+
+                {/* Replace heading with focused input when editing */}
+                {editing ? (
+                    <input defaultValue={title} autoFocus 
+                        onBlur={(event) => {
+                            saveTitle(event.currentTarget.value.trim())
+                        }} 
+                        onKeyDown={(event) => {
+                            if (event.key === 'Enter') {
+                                saveTitle(event.currentTarget.value.trim())
+                            }
+                        }}
+                        onClick={(event) => {
+                            event.preventDefault(); // block link action
+                        }}
+                    />
+                ) : (
+                    <h2>{title}</h2>
+                )}
+
+                <p>#{id}</p>
+            </div>
+
+            <div className="collection-menu-container">
+                <button className="collection-menu-button" onClick={(event) => {
+                    event.preventDefault(); // block link action
+                    onMenuToggle();
+                }}>
+                    ⋮
+                </button>
+
+                {/* Only render menu when open */}
+                {menuOpen && (
+                    <div className="collection-menu">
+                        <button onClick={(event) => {
+                            event.preventDefault(); // block link action
+                            setEditing(true);
+                            onMenuToggle();
+                        }}
+                        >
+                            Rename
+                        </button>
+                        <button className="delete-button" onClick={(event) => {
+                            event.preventDefault(); // block link action
+                            onDelete(id);
+                        }}>
+                            Delete
+                        </button>
+                    </div>
+                )}
+            </div>
         </Link>
     )
 }
--- src/pages/AllCollectionsPage.tsx
+++ src/pages/AllCollectionsPage.tsx
@@ -1,38 +1,79 @@
 import CollectionCard from '../components/CollectionCard'
 import type { Collection } from '../types/Collection'
-
-// testing data
-const collections: Collection[] = [
-    {
-        id: 'abc123',
-        title: 'Work',
-    },
-    {
-        id: 'def456',
-        title: 'Personal',
-    },
-    {
-        id: 'ghi789',
-        title: 'Projects',
-    },
-]
+import './AllCollectionsPage.css'
+import { useState } from 'react'
 
 function AllCollectionsPage() {
+    const [collections, setCollections] = useState<Collection[]>([    
+        {
+            id: 'abc123',
+            title: 'Work',
+        },
+        {
+            id: 'def456',
+            title: 'Personal',
+        },
+        {
+            id: 'ghi789',
+            title: 'Projects',
+        },
+    ]);
+
+    const [openCollectionId, setOpenCollectionId] = useState<string | null> (null) // returns nullable string
+
+    function addCollection() {
+        const newCollection: Collection = {
+            id: crypto.randomUUID(),
+            title: 'New Collection'
+        };
+
+        setCollections([...collections, newCollection]);
+    }
+
+    function deleteCollection(id: string) {
+        setCollections(collections.filter((collection) => collection.id !== id));
+    }
+
+    function renameCollection(id: string, newTitle: string) {
+        setCollections(collections.map((collection) => {
+            if (collection.id === id) {
+                return { ...collection, title: newTitle }
+            }
+
+            return collection;
+        }))
+    }
+
+    function toggleCollectionMenu(id: string) {
+        if (openCollectionId === id) {
+            setOpenCollectionId(null)
+        } else {
+            setOpenCollectionId(id)
+        }
+    }
+    
     return (
         <main>
             <h1>All Collections</h1>
-            
-            <div>
+
+            <div id="collections-list">
                 {collections.map((collection) => (
                     <CollectionCard
-                        key={collection.id}
-                        id={collection.id}
-                        title={collection.title}
-                        noteCount={0} // to replace
+                        key = {collection.id}
+                        id = {collection.id}
+                        title = {collection.title}
+                        menuOpen = {openCollectionId === collection.id}
+                        onMenuToggle = {() => {
+                            toggleCollectionMenu(collection.id)
+                        }}
+                        onDelete = {deleteCollection}
+                        onRename = {renameCollection}
                     />
                 ))}
 
-                <button id="add-collection-button">Add Collection</button>
+                <button id="add-collection-button" onClick={addCollection}>
+                    Add Collection
+                </button>
             </div>
         </main>
     )
